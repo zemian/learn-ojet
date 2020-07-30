fc9501cf89ed6440eda9a81e38151700f0f6ca63
Zemian Deng <zemian.deng@oracle.com>
2020-07-30 10:56:32 -0400

    Add OJET examples

---
+++ static/requirejs-ojet.js
@@ -0,0 +1,21 @@
+requirejs.config({
+    baseUrl: '../../web/js',
+    paths: {
+        'knockout': 'libs/knockout/knockout-3.5.1.debug',
+        'jquery': 'libs/jquery/jquery-3.5.1',
+        'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+        'hammerjs': 'libs/hammer/hammer-2.0.8',
+        'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.2',
+        'ojs': 'libs/oj/v9.0.0/debug',
+        'ojL10n': 'libs/oj/v9.0.0/ojL10n',
+        'ojtranslations': 'libs/oj/v9.0.0/resources',
+        'text': 'libs/require/text',
+        'signals': 'libs/js-signals/signals',
+        'customElements': 'libs/webcomponents/custom-elements.min',
+        'proj4': 'libs/proj4js/dist/proj4-src',
+        'css': 'libs/require-css/css',
+        'touchr': 'libs/touchr/touchr',
+        'corejs': 'libs/corejs/shim',
+        'regenerator-runtime': 'libs/regenerator-runtime/runtime'
+    }
+});
